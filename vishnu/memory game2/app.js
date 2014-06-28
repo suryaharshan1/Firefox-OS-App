13781b7f8522d7989fb46902a8dc8c764a11a641
vishnupriyam <mvpnov1994@gmail.com>
2014-06-28 07:44:12 +0530

    unit converter + memory game + sip texting.

---
+++ vishnu/memory game2/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+        $("#btnPlay").click(function(){
+        //Change to the add-notes
+        //$.mobile.autoInitializePage = true;
+        $.mobile.changePage ($("#play"));
+      });
+
+
+     }); 
